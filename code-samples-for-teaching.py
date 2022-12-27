dbf1d0ee55ef51fe5985cfaec2574821c5b17538
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2022-12-27 22:53:59 +0100

    Update code-samples-for-teaching.py

--- code-samples-for-teaching.py
+++ code-samples-for-teaching.py
@@ -31,3 +31,45 @@ else:
   
 print(name,"asks:", question)
 print("Magic 8-Ball's answer:", answer)
+
+
+
+#####################################################
+
+#shipping costs calculator
+
+weight = 4.8
+
+# Ground Shipping
+cost_ground = 0
+if weight <= 2:
+  cost_ground = weight * 1.5 + 20
+elif weight > 2 and weight < 6:
+  cost_ground = weight * 3.0 + 20
+elif weight > 6 and weight <= 10:
+  cost_ground = weight * 4.0 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+  
+#print("ground Shipping: ", cost_ground)
+
+
+# Premium Shipping
+cost_premium = 125.00
+#print("premium Shipping:", cost_premium)
+
+
+# Drone Shipping
+cost_drone = 0
+if weight <= 2:
+  cost_drone = weight * 4.50
+elif weight > 2 and weight < 6:
+  cost_drone = weight * 9.00
+elif weight > 6 and weight <= 10:
+  cost_drone = weight * 12.0
+else:
+  cost_drone = weight * 14.25
+
+#print("Drone Shipping:", cost_drone)
+
+#####################################################
