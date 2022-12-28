55250e8d496d8fb8e7aa043cba34ef18dcc243aa
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2022-12-28 11:53:39 +0100

    Update code-samples-for-teaching.py

--- code-samples-for-teaching.py
+++ code-samples-for-teaching.py
@@ -73,3 +73,23 @@ else:
 #print("Drone Shipping:", cost_drone)
 
 #####################################################
+
+# Example code for a short gradebook
+semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+ 
+subjects = ["physics","calculus","poetry","history"]
+grades = [98,97,85,88]
+gradebook = [[subjects[0], grades[0]], [subjects[1], grades[2]], [subjects[2], grades[2]], [subjects[3], grades[3]]]
+#print(gradebook)
+
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
+gradebook[-1][1] += 5 
+#print(gradebook)
+
+
+gradebook[2].remove(85)
+gradebook[2].append("Pass")
+#print(gradebook)
+
+#####################################################
