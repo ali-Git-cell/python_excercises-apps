9288a6b6424cd71b593cf39fe8dec3c4e07ae7c0
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2022-12-29 23:49:41 +0100

    Update code-samples-for-teaching.py

--- code-samples-for-teaching.py
+++ code-samples-for-teaching.py
@@ -94,3 +94,34 @@ gradebook[2].append("Pass")
 
 
 #####################################################
+
+# Pizza shop example
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+num_pizzas = len(toppings)
+print("We sell {} different kinds of pizza!".format(num_pizzas))
+
+pizza_and_prices = []
+
+for i in range(num_pizzas):
+  listopt = []
+  listopt.append(prices[i])
+  listopt.append(toppings[i])
+  pizza_and_prices.append(listopt)
+  
+pizza_and_prices.sort()
+
+cheapest_pizza = pizza_and_prices[0]
+priciest_pizza = pizza_and_prices[-1]
+pizza_and_prices.pop(-1)
+
+pizza_and_prices.insert(4, [2.5, "peppers"])
+#print(pizza_and_prices)
+
+three_cheapest = pizza_and_prices[:3]
+#print(three_cheapest)
+
+
+#####################################################
