3df5f21705c6d3e2b758aa61eecf07ea670bfd1c
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2022-12-30 22:49:54 +0100

    Update code-samples-for-teaching.py

--- code-samples-for-teaching.py
+++ code-samples-for-teaching.py
@@ -179,3 +179,53 @@ def destination_setup(origin, destination, estimated_time, mode_of_transport="Ca
   print("It will take approximately {} hours".format(str(estimated_time)))
 
 destination_setup("Delhi", "Mumbai", estimate)
+
+
+#######################################################################################
+
+# calculating fundamental physical properties with the help of pyhton functions
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+
+# Write your code below: 
+
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+f100_in_celsius = f_to_c(100)
+#print(f100_in_celsius)
+
+def c_to_f(c_temp):
+  f_temp = c_temp * (9/5) + 32
+  return f_temp
+#print(c_to_f(0))
+
+c0_in_fahrenheit = c_to_f(0)
+
+def get_force(mass, acceleration):
+  return (mass * acceleration)
+
+train_force = get_force(train_mass, train_acceleration)
+
+#print("The GE train supplies {} Newtons of force.".format(train_force))
+
+def get_energy(mass, c=3*10**8):
+  return (mass * c)
+
+bomb_energy = get_energy(bomb_mass)
+
+#print("A 1kg bomb supplies {} Joules.".format(bomb_energy))
+
+def get_work(mass, acceleration, distance):
+  return get_force(mass, acceleration) * distance
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+#print("The GE train does {} Joules of work over {} meters.".format(train_work, train_distance))
+
+#######################################################################################
+
+
